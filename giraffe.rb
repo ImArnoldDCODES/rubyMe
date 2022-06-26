5e32c7db72acbb96237f487d2143419404ef56d9
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-06-26 13:18:24 -0700

    variable types in ruby

--- giraffe.rb
+++ giraffe.rb
@@ -3,10 +3,19 @@
 # puts " /  |"
 # puts "/___|"
 
-character_name = "John"
-character_age = "35"
+# character_name = "John"
+# character_age = "32"
 
-puts ("There was man named " + character_name)
-puts ("he was " + character_age +" 35 years old.")
-puts ("He really liked the name " + character_name )
-puts ( "but didn't like being 35." + character_age )
+# puts ("There was man named " + character_name)
+# puts ("he was " + character_age +" 35 years old.")
+# character_name = "Tony"
+# puts ("He really liked the name " + character_name )
+# puts ( "but didn't like being " + character_age )
+
+name = "Mike"
+occupation = "programmer"
+age = 73
+gpa = 3.232
+ismale = true
+istall = false
+flaws = nil
