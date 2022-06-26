a086e0f787f06c3bf9717cdf8d6ea197fdc4d236
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-06-26 13:43:07 -0700

    using strings

--- giraffe.rb
+++ giraffe.rb
@@ -1,24 +1,3 @@
-# puts "   /|"
-# puts "  / |"
-# puts " /  |"
-# puts "/___|"
+num = 18
 
-# character_name = "John"
-# character_age = "32"
-
-# puts ("There was man named " + character_name)
-# puts ("he was " + character_age +" 35 years old.")
-# character_name = "Tony"
-# puts ("He really liked the name " + character_name )
-# puts ( "but didn't like being " + character_age )
-
-# name = "Mike"
-# occupation = "programmer"
-# age = 73
-# gpa = 3.232
-# ismale = true
-# istall = false
-# flaws = nil
-
-phrase = "Girraffe Academy"
-puts phrase.index("fe") 
+puts ("My age is " + num.to_s)
