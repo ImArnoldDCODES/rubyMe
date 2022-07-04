e3f12a1e210e40878f142a2808450d17ac86084d
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-07-03 20:37:08 -0700

    3rd July

--- giraffe.rb
+++ giraffe.rb
@@ -5,8 +5,5 @@ puts "Enter Your Age: "
 Age = gets.chomp()
 
 
-puts "Enter your surname: "
-surname = gets.chomp();
 
-
-puts ("Hello " + name +", you are " + Age )
+puts ("Hello " + name", you are " + Age )
